7192c5b92e3ea7e6843c665612cd50895a3dc785
slkaczma <celesiroth@gmail.com>
2015-07-05 13:49:25 -0500

    Updated copyright to reflect ibm-iot-quickstart usage

--- main.js
+++ main.js
@@ -1,26 +1,27 @@
-/*
- * Author: Sarah Knepper <sarah.knepper@intel.com>
- * Copyright (c) 2014 Intel Corporation.
- *
- * Permission is hereby granted, free of charge, to any person obtaining
- * a copy of this software and associated documentation files (the
- * "Software"), to deal in the Software without restriction, including
- * without limitation the rights to use, copy, modify, merge, publish,
- * distribute, sublicense, and/or sell copies of the Software, and to
- * permit persons to whom the Software is furnished to do so, subject to
- * the following conditions:
- *
- * The above copyright notice and this permission notice shall be
- * included in all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
- * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
- * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
- * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
- * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
- * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- */
+// Copyright (c) 2015 IBM Corporation and other Contributors.
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+// Contributors:
+//  IBM - Initial Contribution
+//  Stefania Kacmarczyk (slkaczma@us.ibm.com) 
  
 // Load Grove and MQTT modules
 var groveSensor = require('jsupm_grove');
@@ -28,9 +29,8 @@ var mqtt = require('mqtt');
 var fs = require('fs');
 //var LCD = require('jsupm_i2clcd');
 
-// Create the temperature sensor object using AIO pin 0
+// Initiate new temperature sensor on analog pin 0
 var temp = new groveSensor.GroveTemp(0);
-console.log(temp.name());
 
 // Set up variables for connection to IoTF
 var configFile = "/node_app_slot/device.cfg";
@@ -133,11 +133,11 @@ function sendMessage() {
     message.d = {};
 
     // Read the temperature and send the data to IoTF
-    var celsius = temp.value();
-    var fahrenheit = celsius * 9.0 / 5.0 + 32.0;
+    var c = temp.value();
+    var f = c * 9.0 / 5.0 + 32.0;
 
-    message.d.celsius = celsius;
-    message.d.fahrenheit = fahrenheit;
+    message.d.celsius = c;
+    message.d.fahrenheit = f;
 
     console.log(message);
 
